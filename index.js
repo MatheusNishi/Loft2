496173f2ebd50e106316cdf1961b96e187d6efe5
MatheusNishi <nishikawa@alunos.utfpr.edu.br>
2021-12-11 15:27:48 -0300

    Mais implementacoes

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 let http = require ('http'),
     express = require ('express'),
     path = require ('path'),
+    Post = require ('./model/Post'),
+    User = require ('./model/User'),
     app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
 
-app.get('/a', (req, res) =>{
+app.get('/', (req, res) =>{
     res.render('index');
 });
-app.get('/', (req, res) =>{
-    res.end('<h1>Pagina</h1>');
+app.get('/a', (req, res) =>{
+    res.end('<h1>Pagina a</h1>');
+});
+app.get('/busca', async (req, res) =>{
+    const busca = req.query.busca;
+    const posts = await Post.find(busca);    
+    //let busca = req.query.busca;
+    res.render('busca', { posts: posts });
+    //res.end();
+});
+app.post('/busca', async (req, res) => {
+    const texto = req.body.texto;
+    Post.insert(texto);
+    res.redirect('busca');
+});
+app.post('/login', (req, res) => {
+    let login = req.body.login;
+    let pass = req.body.password;
+    res.end();
 });
 
-http.createServer(app).listen(3003);
+http.createServer(app).listen(8008);
 
 // const MongoClient = require('mongodb').MongoClient;
 
