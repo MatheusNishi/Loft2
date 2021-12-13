52348a5cd4f51027b409f4e9498ae35008e9016d
MatheusNishi <nishikawa@alunos.utfpr.edu.br>
2021-12-12 21:31:14 -0300

    endereco banco

--- model/User.js
+++ model/User.js
@@ -10,12 +10,9 @@ module.exports = class User {
     //         });
     // }
     static async criarConta(email, senha) {
-        const conn = await MongoClient.connect('mongodb://localhost:27017/mongo-test');
+        const conn = await MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mongo-test');
         const db = conn.db();
-        console.log(email);
-        console.log(senha);
         let busca1 = (await db.collection('users').find({ email: email }).toArray()).length;
-        console.log(await db.collection('users').find({ email: email }).toArray());
         if (busca1) {
             conn.close();
             return false;
@@ -27,10 +24,9 @@ module.exports = class User {
     }
 
     static async logarConta(email,senha) {
-        const conn = await MongoClient.connect('mongodb://localhost:27017/mongo-test');
+        const conn = await MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mongo-test');
         const db = conn.db();
         let retorno = (await db.collection('users').find({ email: email, senha: senha }).toArray()).length;
-        console.log(retorno);
         if (retorno) {
             conn.close();
             return true;
